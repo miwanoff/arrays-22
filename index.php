aec3003f7cf7bc32529ffcf6639ddb91c231d9c1
miwanoff <marina.iva.noffa@gmail.com>
2022-09-25 15:44:03 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+// $users1 = ["John" => "qwerty", "Nicole" => "asdf", "Mark" => "ww"];
+// $users2 = ["Joan" => "1234", "Mark" => "poiu", "Nicole" => "ggg"];
+// print_r(array_merge($users1, $users2));
+
+$countries = [
+    "Belgium" => [
+        "French",
+        "Dutch",
+        "German",
+    ],
+    "United Kingdom" => "English",
+    "Switzerland" => [
+        "German",
+        "French",
+        "Italian",
+    ],
+    "Germany" => "German",
+    "Italy" => "Italian",
+];
+
+echo "Сountries\n\n";
+
+foreach ($countries as $key => $value) {
+    //   if (!is_array ( $value ))
+    //       echo "$key:\n\t$value\n";
+    //   else {
+    //       echo "$key: \n";
+    //       foreach ( $value as $v )
+    //           echo "\t$v\n";
+    //   }
+    //      echo "\n";
+    echo "$key: \n";
+    // print_r($value);
+    if (is_array($value)) {
+        foreach ($value as $v) {
+            echo "$v\t";
+        }
+    } else {
+        echo "$value";
+    }
+
+    echo "\n\n";
+}
+echo "\n\n";
+echo "\n\n";
+$languages = [];
+
+foreach ($countries as $country => $cl) {
+    // if (!is_array ($cl)) {
+    //     $languages[$cl][] = $country;
+    // } else {
+    //     foreach ($cl as $l) {
+    //         $languages [$l] [] = $country;
+    //     }
+    // }
+    if (!is_array($cl)) {
+        $languages[$cl][] = $country;
+    } else {
+        foreach ($cl as $l) {
+            $languages[$l][] = $country;
+        }
+    }
+
+}
+// print_r($languages);
+foreach ($languages as $key => $value) {
+    if (!is_array($value)) {
+        echo "$key:\n\t$value";
+    } else {
+        echo "$key: \n";
+        foreach ($value as $v) {
+            echo "\t$v\n";
+        }
+
+    }
+}
+echo "\n\n";
